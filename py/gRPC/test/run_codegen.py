c9063fd7c8727f35985e9621d1a78bd8c5a47bec
Shlomi <Shlomi@Home.il>
2018-11-03 10:58:00 +0200

    Add note for install grpc dependencies

--- py/gRPC/test/run_codegen.py
+++ py/gRPC/test/run_codegen.py
@@ -9,3 +9,11 @@ protoc.main((
     '--grpc_python_out=./src',
     './protos/all.proto',
 ))
+
+#region Install Pre-requisites
+
+##requires intallation of grpc and proto tools =>  
+# python -m pip install grpcio 
+# python -m pip install grpcio-tools googleapis-common-protos
+
+#endregion
